b34d061893725b30dfda07986c86ee2302a45e58
Roman <roman.elokhin@global.ntt>
2020-10-04 22:13:32 +0000

    -

--- ntest20.py
+++ ntest20.py
@@ -2,6 +2,18 @@ import networkx as nx
 from networkx.drawing.nx_agraph import write_dot, graphviz_layout
 import matplotlib.pyplot as plt
 
+
+def color(G):
+    color_list = []
+    for node in list(G.nodes):
+        path = nx.shortest_path_length(G, source="ROOT", target=node, weight=1, method='dijkstra')
+        if int(path) % 2:
+            color_list.append("gold")
+        else:
+            color_list.append("violet")
+    return color_list
+
+
 G = nx.DiGraph()
 G.add_node("ROOT")
 G.add_node("TEST 1")
@@ -16,15 +28,13 @@ G.add_edge("TEST 1", "TEST 3")
 G.add_edge("TEST 2", "TEST 4")
 G.add_edge("TEST 4", "TEST 5")
 
-
-# write dot file to use with graphviz
-# run "dot -Tpng test.dot >test.png"
-write_dot(G,'test.dot')
-
-# same layout using matplotlib with no labels
-plt.title('draw_networkx')
+color_ = color(G)
+# pos = hierarchy_pos(G,1)
 pos =graphviz_layout(G, prog='dot')
-nx.draw(G, pos, with_labels=True, arrows=True)
-# plt.savefig('nx_test.png')
+#nx.draw_networkx_nodes(G, pos=pos,node_color=color_, with_labels=True, node_size=600, node_shape = 's')
+#nx.draw_networkx_labels(G, pos=pos,font_size = 6, font_family='sans-serif')
+#nx.draw_networkx_edges(G, pos=pos)
+nx.draw(G, pos=pos,node_color=color_, with_labels=True, node_size=1600, node_shape = 's', font_size = 6, font_family='sans-serif', bbox = dict(facecolor = "none", edgecolor = "black", boxstyle = 'round,pad=1'))
+plt.savefig('hierarchy.png')
 plt.show()
 
