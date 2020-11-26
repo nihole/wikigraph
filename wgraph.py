64473f264f96e190f21e213973c68dada7c8a479
Roman <roman.elokhin@global.ntt>
2020-11-26 23:23:44 +0000

    -

--- wgraph.py
+++ wgraph.py
@@ -21,32 +21,20 @@ def wgraph(yaml_data, status_dict, graph_file):
         if m1:
             lbl = record['id'] + '\n' + format.format(record['text'])
             if not (record['id'] in status_dict.keys()):
-                g.attr('node', color='red')
+                g.attr('node', color='red', fontcolor = 'black')
                 g.node(record['id'], label=lbl, URL='https://www.ru')
             else:
-                if (status_dict[record['id']] == -1) or (status_dict[record['id']] == 0):
-                    g.attr('node', color='gray90', fontcolor = 'gray90')
-                    g.node(record['id'], label=lbl, URL='https://www.ru')
+                g.attr('node', color='gray90', fontcolor = 'gray90')
+                g.node(record['id'], label=lbl, URL='https://www.ru')
 
-                else:
-                    g.attr('node', color='red')
-                    g.node(record['id'], label=lbl, URL='https://www.ru')
         elif m2:
             lbl = record['id'] + '\n' + format.format(record['text'])
             if not (record['id'] in status_dict.keys()): 
-                g.attr('node', color='blue', href='https://www.ru')
+                g.attr('node', color='blue', href='https://www.ru',  fontcolor = 'black')
                 g.node(record['id'], label=lbl)
             else:
-                if (status_dict[record['id']] == -1) or (status_dict[record['id']] == 0):
-                    g.attr('node', color='gray90', fontcolor = 'gray90')
-                    g.node(record['id'], label=lbl, URL='https://www.ru')
-                else:
-                    g.attr('node', color='blue', href='https://www.ru')
-                    g.node(record['id'], label=lbl)
-        else:
-            lbl = record['id'] + '\n' + format.format(record['text'])
-            g.attr('node', color='none')
-            g.node(record['id'], label=lbl)
+                g.attr('node', color='gray90', fontcolor = 'gray90')
+                g.node(record['id'], label=lbl, URL='https://www.ru')
 
     ### Edge creation
 
@@ -56,28 +44,19 @@ def wgraph(yaml_data, status_dict, graph_file):
                 if not (edge['id'] in status_dict.keys()):
                     g.edge(record['id'], edge['id'])
                 else:
-                    if (status_dict[edge['id']] == -1) or  (status_dict[edge['id']] == 0):
-                        g.edge(record['id'], edge['id'], color='gray90')
-                    else:
-                        g.edge(record['id'], edge['id'])
+                    g.edge(record['id'], edge['id'], color='gray90')
         for edge in record['indirect_contradictions']:
             if edge['id']:
                 if not (edge['id'] in status_dict.keys()):
                     g.edge(record['id'], edge['id'], style='dotted')
                 else:
-                    if (status_dict[edge['id']] == -1) or  (status_dict[edge['id']] == 0):
-                        g.edge(record['id'], edge['id'], color='gray90',style='dotted')
-                    else:
-                        g.edge(record['id'], edge['id'], style='dotted')
+                    g.edge(record['id'], edge['id'], color='gray90',style='dotted')
         for edge in record['complement']:
             if edge['id']:
                 if not (edge['id'] in status_dict.keys()):
                     g.edge(record['id'], edge['id'], color='red', dir="both")
                 else:
-                    if (status_dict[edge['id']] == -1) or  (status_dict[edge['id']] == 0):
-                        g.edge(record['id'], edge['id'], color='gray90', dir="both")
-                    else:
-                        g.edge(record['id'], edge['id'], color='red', dir="both")
+                    g.edge(record['id'], edge['id'], color='gray90', dir="both")
 
     g.render(view=False)  
 #   g.view()
