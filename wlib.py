f3beab850a6b62aac1167f0d4f7655037ceaa2ba
Roman <roman.elokhin@global.ntt>
2020-12-20 00:23:25 +0000

    -

--- wlib.py
+++ wlib.py
@@ -163,35 +163,28 @@ def paths_to_root (id_, root_id,  rdict,  reverse_rdict):
     return paths_to_root
 
 
-def path_check (id_, rdict,  reverse_rdict, status_dict):
+def path_check (id_, root_id, rdict,  reverse_rdict, status_dict):
 ### If we know the status of node with id = id_ (1 - true, 0 - false, -1 - deleted)
 ### then next step downstream nodes with only one reverse path to root via this
 ### node (id = id_) should be marked as deleted (-1)
     
     ### initiation of lists with such nodes for direct, inderect, complement edges:
     status_dict[id_] = -1
-    dnode_list = []
-    inode_list = []
-    cnode_list = []
+    flag = 0
+    node_list = []
     for dnode_ in list(set(rdict[id_]['direct'] + rdict[id_]['indirect'] + rdict[id_]['complement'])):
-        if (not dnode_ in status_dict.keys()) or ((dnode_ in status_dict.keys()) and not ( status_dict[dnode_] == -1)):
-            all_reverse_node = list(set(reverse_rdict[dnode_]['direct'] + reverse_rdict[dnode_]['indirect'] + reverse_rdict[dnode_]['complement']))
-            flag = 0
-            for reverse_node_ in all_reverse_node:
-                if (reverse_node_ in status_dict.keys()) and not ( status_dict[reverse_node_] == -1):
-                    flag = 1
-                    break
+        paths_to_root_ = paths_to_root (dnode_, root_id,  rdict,  reverse_rdict)
+        for paths in paths_to_root_:
+            if not id_ in paths:
+                flag = 1
+                break
+        if not flag:
+            status_dict[dnode_] = -1
+            node_list.append(dnode_)
 
-            if not flag:
-                status_dict[dnode_]=-1
-                dnode_list.append(dnode_)
-    ### return the list of removed downstream nodes and new status_dict
+    return (node_list, status_dict)
 
-        return (dnode_list + inode_list + cnode_list, status_dict)
-    else:
-        return ([], status_dict)
-
-def recurse_path_check (id_, rdict,  reverse_rdict, status_dict):
+def recurse_path_check (id_, root_id, rdict,  reverse_rdict, status_dict):
     import copy
 ### using step by step recursion (path_check) marks as deleted all downstram nodes
 ### having a single path to root via the node with id = id_
@@ -203,7 +196,7 @@ def recurse_path_check (id_, rdict,  reverse_rdict, status_dict):
         ### this will be commulative node list
         for node in node_list:
             node_l = []
-            (node_l, status_dict) = path_check(node, rdict,  reverse_rdict, status_dict)
+            (node_l, status_dict) = path_check(node, root_id, rdict,  reverse_rdict, status_dict)
             node_lst = node_lst + node_l
             node_lst = list(dict.fromkeys(node_lst))
         node_list = copy.copy(node_lst)
@@ -214,7 +207,7 @@ def recurse_path_check (id_, rdict,  reverse_rdict, status_dict):
             break
     return status_dict
 
-def node_resolving (id_, rdict, reverse_rdict, status_dict):
+def node_resolving (id_, root_id, rdict, reverse_rdict, status_dict):
 ## return dictionary ststus_dict. Keys - ids of nodes (statements), values: 1,-1,0, 777 
 ### 1 means true, 0 - false, -1 - lost path to the root. 777 is used to label a conflict (true/false) 
     
@@ -222,7 +215,7 @@ def node_resolving (id_, rdict, reverse_rdict, status_dict):
     status_dict[id_] = 1
     ### mark as deleted (-1) all downstreem nodes (for all types of edge) 
     ### if they have path to root only via this node
-    status_dict = recurse_path_check(id_, rdict,  reverse_rdict, status_dict)
+    status_dict = recurse_path_check(id_, root_id, rdict,  reverse_rdict, status_dict)
 
     reverse_direct_and_complement = reverse_rdict[id_]['direct']+reverse_rdict[id_]['complement']
     for dnode in reverse_direct_and_complement:
@@ -282,9 +275,9 @@ if __name__ == "__main__":
     (dict1, dict2) = recurs_dict(yaml_data)
     dd = deadend(yaml_data, {})
 
-    paths = paths_to_root ('--19--', '++0++', dict1, dict2)
-    print (paths)
-#    status_dict_ = node_resolving(id_dep, dict1, dict2, {})
-#    print status_dict_
+#    paths = paths_to_root ('--19--', '++0++', dict1, dict2)
+#    print (paths)
+    status_dict_ = node_resolving(id_dep, '++0++', dict1, dict2, {})
+    print status_dict_
 
-#    wgraph.wgraph(yaml_data, status_dict_, 'desdemona')
+    wgraph.wgraph(yaml_data, status_dict_, 'desdemona')
